5b5fd8d9ac032a11c4c5f01e87e619b3684749c4
eloirotava <46318465+eloirotava@users.noreply.github.com>
2021-06-08 13:51:33 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/eloirotava/lakka-2.3.2.git temp && cp -r temp/* . && rm -rf temp && make image PROJECT=S805 ARCH=arm DISTRO=Sx05RE SYSTEM=HD18Q
+git clone https://github.com/eloirotava/lakka.git temp && cp -r temp/* . && rm -rf temp && DISTRO=Lakka PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j2
